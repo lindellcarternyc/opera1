400378cb8b973386ec445fc2c0766074a6b6a91c
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-07 19:58:06 -0500

    Replaced boilerplace in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 // @flow
 
-// import React, { Component } from 'react'
 import * as React from 'react'
-// import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-class App extends React.Component {
+class App extends React.Component<null> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1>Opera App</h1>
     );
   }
 }
