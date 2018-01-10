baaf1c1220e2a8c825acf74fd683468c6da1e7f7
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-09 22:23:55 -0500

    Added defaultProps including dummy opera

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import * as React from 'react'
 import './App.css'
 
 type Props = {
+  opera: {
+    title: string
+  }
 }
 
 class App extends React.Component<Props> {
+  static defaultProps: Props = {
+    opera: {
+      title: 'Rigoletto'
+    }
+  }
+  constructor(props: Props) {
+    super(props)
+  }
   render() {
+    const { opera } = this.props
     return (
-      <h1>Opera App</h1>
+      <h1>{opera.title}</h1>
     );
   }
 }
